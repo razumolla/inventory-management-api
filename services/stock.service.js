5d188a6023b5de7bf8399600f9de20855ab2a80a
razumolla <rajuahmed9344@gmail.com>
2023-04-01 07:52:27 +0600

    aggregate operations

--- services/stock.service.js
+++ services/stock.service.js
@@ -4,11 +4,31 @@ const ObjectId = mongoose.Types.ObjectId;
 
 exports.getStocksService = async (filters, queries) => {
 
-  const stocks = await Stock.find(filters)
-    .skip(queries.skip)
-    .limit(queries.limit)
-    .select(queries.fields)
-    .sort(queries.sortBy)
+  // const stocks = await Stock.find(filters)
+  //   .skip(queries.skip)
+  //   .limit(queries.limit)
+  //   .select(queries.fields)
+  //   .sort(queries.sortBy)
+
+  // get information by aggregate --
+  const stocks = await Stock.aggregate([
+    // { $match: { 'store.name': 'chattogram' } }
+    { $match: {} },
+    {
+      $project: {
+        store: 1,
+        price: { $convert: { input: '$price', to: 'int' } },
+        quantity:1
+    } },
+    {
+      $group: {
+        _id: '$store.name',
+        totalProductPrice: { $sum: { $multiply: ["$price", "$quantity"] } }
+      }
+    }
+
+  ])
+
 
   const total = await Stock.countDocuments(filters);
   const page = Math.ceil(total / queries.limit);
@@ -20,10 +40,41 @@ exports.getStocksService = async (filters, queries) => {
 
 
 exports.getStockByIdService = async (id) => {
-  const stock = await Stock.findOne({ _id: id })
-    .populate("store.id")
-    .populate("suppliledBy.id")
-    .populate("brand.id");
+//   const stock = await Stock.findOne({ _id: id })
+//     .populate("store.id")
+//     .populate("suppliledBy.id")
+//     .populate("brand.id");
+    
+    // now we create aggregation pipe line
+    // popeline -> many stages
+    // data -> one stage -> two stage
+    //        name, age, contactNo --> contactNo
+  
+    const stock = await Stock.aggregate([
+        // stage-1
+        { $match: { _id: new ObjectId(id) } },
+        { 
+            $project: {
+                category: 1,
+                price:1,
+                quantity:1,
+                productId:1,
+                name: 1,
+                'brand.name': { $toLower: '$brand.name' }
+            }
+        },
+        {
+            $lookup: {
+                from: 'brands',
+                localField: 'brand.name',
+                foreignField: 'name',
+                as: 'brandDetails'
+            }
+      }
+    ])
+  
+    
+    
   return stock;
 };
 
