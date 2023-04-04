b82db9e1518870d68f837fa48940d04207a86459
razumolla <rajuahmed9344@gmail.com>
2023-04-04 23:30:02 +0600

    user authorization with verifyToken

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
 const uploader = require('../middleware/uploader');// for file upload system
+const verifyToken = require("../middleware/verifyToken");
+const authorization = require("../middleware/authorization")
+
+// router.use(verifyToken)
 
 // router.post("/file-upload", uploader.single("image"), productController.fileUpload)
 router.post("/file-upload", uploader.array("image"), productController.fileUpload)
@@ -16,11 +20,11 @@ router.route("/bulk-delete").delete(productController.bulkDeleteProduct); // mul
 router
   .route("/")
   .get(productController.getProducts)
-  .post(productController.createProduct);
+  .post(verifyToken, authorization("admin","store-manager"), productController.createProduct);
 
 router
   .route("/:id")
   .patch(productController.updateProductById)
-  .delete(productController.deleteProductById);
+  .delete(verifyToken, authorization("admin"),productController.deleteProductById);
 
 module.exports = router;
